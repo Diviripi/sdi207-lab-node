8187efe69192c89b9008a8b7e642e31b395f5b53
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-03-21 12:42:54 +0100

    “SDI-NodeJS-.3,4,5 Express, depuración y varibles de entorno

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-var http = require('http');
-http.createServer(function handler(req, res) {
- res.writeHead(200, {'Content-Type': 'text/plain'});
- res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+// Módulos
+var express = require('express');
+var app = express();
+// Variables
+app.set('port', 8081);
+app.get('/usuarios', function(req, res) {
+res.send('ver usuarios');
+})
+app.get('/canciones', function(req, res) {
+res.send('ver canciones');
+})
+// lanzar el servidor
+app.listen(app.get('port'), function() {
+console.log("Servidor activo");
+})
