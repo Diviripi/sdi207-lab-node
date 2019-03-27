ba575bcd7cb5af1d603849f4bcb17d946a050612
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-03-27 17:42:19 +0100

    Added mongo url

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+/*
+mongodb://admin:sdi@tiendamusica-shard-00-00-jnakn.mongodb.net:27017,tiendamusica-shard-00-01-jnakn.mongodb.net:27017,tiendamusica-shard-00-02-jnakn.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true
+*/
 // Variables
 app.set('port', 8081);
 app.use(express.static('public'));
