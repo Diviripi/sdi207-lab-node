062844ba66f24ab13f5f6f61c33e104c19c55868
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-03-27 17:53:02 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 /*
 mongodb://admin:sdi@tiendamusica-shard-00-00-jnakn.mongodb.net:27017,tiendamusica-shard-00-01-jnakn.mongodb.net:27017,tiendamusica-shard-00-02-jnakn.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true
 */
@@ -18,8 +21,8 @@ app.use(express.static('public'));
 
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig,mongo); 
+require("./routes/rusuarios.js")(app, swig,gestorBD);
+require("./routes/rcanciones.js")(app, swig,gestorBD); 
 // lanzar el servidor
 app.listen(app.get('port'), function() {
 console.log("Servidor activo");
